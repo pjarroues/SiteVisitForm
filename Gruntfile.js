9d9d5f35e366ae3b088e0ad6c4b571c425701947
pjarroues <pjarroues@users.noreply.github.com>
2014-09-15 13:57:28 -0600

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,35 @@
+'use strict';
+
 module.exports = function(grunt) {
 // Project configuration.
 grunt.initConfig({
-pkg: grunt.file.readJSON('package.json'),
- jscs: {
+@@ -6,13 +8,31 @@ module.exports = function(grunt) {
+options: {
+config: '.jscsrc',
+},
+all: [
+ '<%= jshint.build.src %>',
+ '<%= jshint.app.src %>',
+ ],
+ },
+ jshint: {
+ build: {
  options: {
- config: '.jscsrc',
+ jshintrc: '.jshintrc',
  },
- all: ['*.js', 'src/**/*.js'],
+ src: '*.js',
  },
+ app: {
+ options: {
+ jshintrc: 'src/.jshintrc',
+ },
+ src: 'src/**/*.js',
+ },
+},
 });
  grunt.loadNpmTasks('grunt-jscs');
-
+ grunt.loadNpmTasks('grunt-contrib-jshint');
 // Register alias tasks.
-grunt.registerTask('default', ['jscs']);
+grunt.registerTask('default', ['jscs', 'jshint']);
 
 };
